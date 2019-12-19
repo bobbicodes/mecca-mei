238172d7392b3a8dc17163098d42481e050be07f
porkostomus <porkostomus@mail.com>
2019-12-18 21:18:34 -0800

    render SVG with Verovio

--- src/mecca/mei.cljs
+++ src/mecca/mei.cljs
@@ -23,6 +23,9 @@
   </measure>
 </section>"))
 
+(defn svg-out []
+  [:div.svg {:dangerouslySetInnerHTML {:__html (.renderData js/vrvToolkit @file-atom)}}])
+
 (defn file-upload []
   [:div
    [:h3 "Import .mei score"]
@@ -74,38 +77,47 @@
 (defn attr= [a v]
   (attrp a (partial = v)))
 
+(defn button [label onclick]
+  [:button
+   {:on-click onclick}
+   label])
+
+(def root (r/atom "root"))
+
 (defn render-tags []
-  [:div
-   [:h3 "Tags:"]
-   [:textarea
-    {:rows      5
-     :cols      50
-     :value     (str (sequence tags [(js->clj (.parseString xml @file-atom) :keywordize-keys true)]))
-     :read-only true}]])
+  (let [doc (js->clj (.parseString xml @file-atom) :keywordize-keys true)]
+    [:div
+     [:h3 (str "[" @root "]")]
+     (for [tag (sequence tags [doc])]
+       ^{:key tag}
+       [button tag #(reset! root tag)])]))
 
 (defn render-children []
-  [:div
-   [:h3 "Children:"]
-   [:textarea
-    {:rows      15
-     :cols      50
-     :value     (str (sequence children [(js->clj (.parseString xml @file-atom) :keywordize-keys true)]))
-     :read-only true}]])
+  (let [doc (js->clj (.parseString xml @file-atom) :keywordize-keys true)]
+    [:div
+     [:textarea
+      {:rows      15
+       :cols      50
+       :value     (str (sequence (tag= @root) [doc]))
+       :read-only true}]]))
 
 (comment
+
+  (let [doc  (js->clj (.parseString xml @file-atom) :keywordize-keys true)
+        path [(tag= "section") children]]
+    (tag= "mei") [doc])
+
   (sequence children [(js->clj (.parseString xml @file-atom) :keywordize-keys true)])
   (sequence (tag= "measure") [(js->clj (.parseString xml @file-atom) :keywordize-keys true)])
-  
+
   (->> [(js->clj (.parseString xml @file-atom) :keywordize-keys true)]
        (sequence (tag= "measure"))
        count)
-  
+
   (->> [(js->clj (.parseString xml @file-atom) :keywordize-keys true)]
        (sequence (comp (tag= :chapter)
                        (attr= :name "Conclusion")))
-       count)
-  
-  )
+       count))
 
 (defn mecca []
   [:div
@@ -113,10 +125,13 @@
    [:p "Music data browser"]
    [file-upload]
    [:p]
+   [svg-out]
    [mei-out]
    [edn-out]
-   [render-tags]
-   [render-children]])
+   ;[render-tags]
+   ;[render-children]
+   ])
+
 
 (defn get-app-element []
   (gdom/getElement "app"))
