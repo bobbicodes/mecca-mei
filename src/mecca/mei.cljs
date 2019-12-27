dcd10f293c2765ead79b667b74e28e4f7da75262
porkostomus <porkostomus@mail.com>
2019-12-26 21:09:38 -0800

    update live app

--- src/mecca/mei.cljs
+++ src/mecca/mei.cljs
@@ -53,14 +53,6 @@
 
 (defonce file-atom (r/atom (make-mei @hiccup-atom)))
 
-(comment
-
-
-
-  )
-
-
-
 (defn svg-out []
   [:div.svg {:dangerouslySetInnerHTML {:__html (.renderData js/vrvToolkit @file-atom)}}])
 
@@ -173,18 +165,13 @@
   (when-let [el (get-app-element)]
     (mount el)))
 
-;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
   (mount-app-element)
   (js/console.log "start"))
 
 (defn ^:export init []
-  ;; init is called ONCE when the page loads
-  ;; this is called in the index.html and must be exported
-  ;; so it is available even in :advanced release builds
   (js/console.log "init")
   (start))
 
-;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
   (js/console.log "stop"))
